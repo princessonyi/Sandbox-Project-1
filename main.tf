49007089b940a9b883591e9e814176ef0ec415cf
Princessonyi <princessalieze@gmail.com>
2025-03-26 15:05:42 +0000

    all changes made to clearify each line on the code

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "sandboxRG"  # The name of the resource group is explicitly set to "sandboxRG."
-  location = var.resource_group_location  # This gets the location value from the variable, which defaults to "eastus."
+  name     = "sandboxRG"  # The name of the resource group must be "sandboxRG."
+  location = var.resource_group_location  # This picks the location value from the variable file, which is set by defult to "eastus." 
   
-  tags = {  # Tags are key-value pairs, used for organizing resources.
-    "Created by" = "princess"  # Tag to specify the resource group was created by "princess."
+  tags = {  # Tags are key-value, used for organizing resources.
+    "Created by" = "princess"  # This tag is to specify that the resource group was created by "princess."
   }
 }
